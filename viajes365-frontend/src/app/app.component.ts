6da31205a3f6e5f74ea074fc31b92aea189b6ade
Anibal H. Flores <floresanibal@yahoo.com.ar>
2021-03-19 17:57:26 -0300

    Revert "Merge pull request #31 from classroomEntreRios/US#02TK#012_Crear_formulario_de_login"
    This reverts commit f06bf1208f65cb05c858d761225e82e12d21ac41, reversing
    changes made to 47ee1f6d32791f4e00efcc1d25efd17bec9f75bd.

--- viajes365-frontend/src/app/app.component.ts
+++ viajes365-frontend/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'viajes365-frontend';
-
-  logout(): void {
-    // ToDo
-    // this.accountService.logout();
-  }
 }
