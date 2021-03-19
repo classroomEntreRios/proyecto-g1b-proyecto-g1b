37df3996952ca1770f7aa8b73b507780375188e8
Alexx73 <ttllcba@gmail.com>
2021-03-19 10:59:31 -0700

    formulario de login listo

--- viajes365-frontend/src/app/app-routing.module.ts
+++ viajes365-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {LoginComponent} from "./account/login.component";
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+  { path: 'login', redirectTo: 'account/login', pathMatch: 'full' }
 ];
 
 @NgModule({
