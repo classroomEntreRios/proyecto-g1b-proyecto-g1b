e8e38d8cd721dcd332bb2e26f943b2cfbe2c2dac
Anibal H Flores <floresanibal@yahoo.com.ar>
2021-03-21 23:00:06 -0300

    Final commit added sidebar and dashboard

--- viajes365-frontend/src/app/admin/admin.module.ts
+++ viajes365-frontend/src/app/admin/admin.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { DashboardComponent } from './dashboard.component';
 import { SidebarComponent } from './sidebar.component';
-
-
-
+import { AdminRoutingModule } from './admin-routing.module';
 @NgModule({
   declarations: [LayoutComponent, DashboardComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
--- viajes365-frontend/src/app/app-routing.module.ts
+++ viajes365-frontend/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const adminModule = () => import('./admin/admin.module').then(x => x.AdminModule);
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', redirectTo: 'account/login', pathMatch: 'full' },
   { path: 'register', redirectTo: 'account/register', pathMatch: 'full' },
+  { path: 'dasboard', redirectTo: 'admin/dashboard', pathMatch: 'full' },
   { path: 'account', loadChildren: accountModule, pathMatch: 'prefix' },
+  { path: 'admin', loadChildren: adminModule, pathMatch: 'prefix' },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
