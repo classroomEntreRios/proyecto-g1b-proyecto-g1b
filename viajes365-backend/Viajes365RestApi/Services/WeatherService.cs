45e91f6bd7f36b0b11e90646eeff1c40187194ca
Anibal H Flores <floresanibal@yahoo.com.ar>
2021-04-05 18:58:50 -0300

    Init commit added Services/WeatherService.cs

---
+++ viajes365-backend/Viajes365RestApi/Services/WeatherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Viajes365RestApi.Entities;
+
+namespace Viajes365RestApi.Services
+{
+    public interface IWeatherService
+    {
+        Weather GetByCityId(long id);
+    }
+
+    public class WeatherService : IWeatherService
+    {
+        public Weather GetByCityId(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
