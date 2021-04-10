379a16be72d25ae99f6236412db4c58d13b7f6b6
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-10 20:24:46 -0300

    Agrego dto y builder de localidad

---
+++ viajes365-backend/Viajes365RestApi/Dtos/LocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viajes365RestApi.Dtos
+{
+    public class LocationDto
+    {
+        public long LocationId { get; set; }
+        public string LocationName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string FullAddress { get; set; }
+        public byte Note { get; set; }
+    }
+}
