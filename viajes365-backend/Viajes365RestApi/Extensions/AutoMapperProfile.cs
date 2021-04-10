3e841f99cbeb516652c2dd2b8e7b42baff5913f3
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-10 20:25:20 -0300

    Actualizo mapper con localidad

--- viajes365-backend/Viajes365RestApi/Extensions/AutoMapperProfile.cs
+++ viajes365-backend/Viajes365RestApi/Extensions/AutoMapperProfile.cs
@@ -18,6 +18,8 @@ namespace Viajes365RestApi.Extensions
             CreateMap<UserUpdateDto, User>();
             CreateMap<RoleDto, Role>();
             CreateMap<Role, RoleDto>();
+            CreateMap<Location, LocationDto>();
+            CreateMap<LocationDto, Location>();
         }
     }
 }
