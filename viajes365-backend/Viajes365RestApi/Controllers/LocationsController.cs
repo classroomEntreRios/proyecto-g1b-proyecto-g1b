8b667aff07c2d4247808e344a39940322011c8f7
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-10 20:26:10 -0300

    agrego controller de localidad

---
+++ viajes365-backend/Viajes365RestApi/Controllers/LocationsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Viajes365RestApi.Entities;
+using Viajes365RestApi.Extensions;
+using Viajes365RestApi.Helpers;
+
+namespace Viajes365RestApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Authorization(adminrole)]
+    [Route("api/[controller]")]
+    public class LocationsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        const string adminrole = "Administrador";
+
+        public LocationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Locations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        // GET: api/Locations /5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Location>> GetLocation(long id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return location;
+        }
+
+        // PUT: api/locations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLocation(long id, Location location)
+        {
+            if (id != location.LocationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Locations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Location>> PostLocation(Location location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = location.LocationId }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocation(long id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LocationExists(long id)
+        {
+            return _context.Locations.Any(e => e.LocationId == id);
+        }
+    }
+}
