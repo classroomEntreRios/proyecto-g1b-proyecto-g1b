b58f0b6f518a1032f5f1748a535a9631b62b8431
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-11 21:47:28 -0300

    Agrego controller de tour

---
+++ viajes365-backend/Viajes365RestApi/Controllers/ToursController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Viajes365RestApi.Entities;
+using Viajes365RestApi.Extensions;
+using Viajes365RestApi.Helpers;
+
+namespace Viajes365RestApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Authorization(adminrole)]
+    [Route("api/[controller]")]
+    public class ToursController : ControllerBase
+    {
+        private readonly DataContext _context;
+        const string adminrole = "Administrador";
+
+        public ToursController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tours
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tour>>> GetTours()
+        {
+            return await _context.Tours.ToListAsync();
+        }
+
+        // GET: api/Tours/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tour>> GetTour (long id)
+        {
+            var tour = await _context.Tours.FindAsync(id);
+
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            return tour;
+        }
+
+        // PUT: api/tours/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTour(long id, Tour tour)
+        {
+            if (id != tour.TourId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tour).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tours
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tour>> PostTour(Tour tour)
+        {
+            _context.Tours.Add(tour);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTour", new { id = tour.TourId }, tour);
+        }
+
+        // DELETE: api/Tours/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTour(long id)
+        {
+            var tour = await _context.Tours.FindAsync(id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tours.Remove(tour);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TourExists(long id)
+        {
+            return _context.Tours.Any(e => e.TourId == id);
+        }
+    }
+}
