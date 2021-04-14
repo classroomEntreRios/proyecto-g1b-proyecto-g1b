5815019c51805ccf5ddb136ab93a4d75b829cdb5
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-13 23:45:46 -0300

    Agrego entidad attraction

---
+++ viajes365-backend/Viajes365RestApi/Entities/Attraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Viajes365RestApi.Entities
+{
+    public class Attraction : Base
+    {
+        [Key]
+        public long AttractionId { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Note { get; set; }
+        public int Rating { get; set; }
+
+        [ForeignKey("Location")]
+        public long LocationId { get; set; }
+        //public Location Location { get; set; }
+
+        public ICollection<Tour_attraction> Tour_Attractions { get; set; }
+    }
+}
