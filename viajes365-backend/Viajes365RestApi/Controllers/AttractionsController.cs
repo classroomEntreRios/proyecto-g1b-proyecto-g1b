5192236a6668b7022ed1edd3df57b4a80951d341
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-13 23:48:01 -0300

    Agrego controller de attraction

---
+++ viajes365-backend/Viajes365RestApi/Controllers/AttractionsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Viajes365RestApi.Entities;
+using Viajes365RestApi.Extensions;
+using Viajes365RestApi.Helpers;
+
+namespace Viajes365RestApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Authorization(adminrole)]
+    [Route("api/[controller]")]
+    public class AttractionsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        const string adminrole = "Administrador";
+
+        public AttractionsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Attractions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Attraction>>> GetAttractions()
+        {
+            return await _context.Attractions.ToListAsync();
+        }
+
+        // GET: api/Attractions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attraction>> GetAttraction(long id)
+        {
+            var attraction = await _context.Attractions.FindAsync(id);
+
+            if (attraction == null)
+            {
+                return NotFound();
+            }
+
+            return attraction;
+        }
+
+        // PUT: api/Attractions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAttraction(long id, Attraction attraction)
+        {
+            if (id != attraction.AttractionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(attraction).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttractionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Attractions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Attraction>> PostAttraction(Attraction attraction)
+        {
+            _context.Attractions.Add(attraction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAttraction", new { id = attraction.AttractionId }, attraction);
+        }
+
+        // DELETE: api/Attractions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttraction(long id)
+        {
+            var attraction = await _context.Attractions.FindAsync(id);
+            if (attraction == null)
+            {
+                return NotFound();
+            }
+
+            _context.Attractions.Remove(attraction);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AttractionExists(long id)
+        {
+            return _context.Attractions.Any(e => e.AttractionId == id);
+        }
+    }
+}
