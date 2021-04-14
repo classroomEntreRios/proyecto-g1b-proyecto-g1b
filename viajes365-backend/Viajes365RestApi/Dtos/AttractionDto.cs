112ccac7a214179ff59d32c6ad39888ccff5193c
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-04-13 23:46:17 -0300

    Agrego attraction dto

---
+++ viajes365-backend/Viajes365RestApi/Dtos/AttractionDto.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Viajes365RestApi.Dtos
+{
+    public class AttractionDto
+    {
+        public long AttractionId { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Note { get; set; }
+        public int Rating { get; set; }
+
+        public long LocationId { get; set; }
+        //public Location Location { get; set; }
+    }
+}
