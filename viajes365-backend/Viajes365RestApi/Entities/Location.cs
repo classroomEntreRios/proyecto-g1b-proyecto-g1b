32e9c87a396481132e07d824826369f64a5deb14
Anibal H Flores <floresanibal@yahoo.com.ar>
2021-04-18 11:44:10 -0300

    added missing entities

---
+++ viajes365-backend/Viajes365RestApi/Entities/Location.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Viajes365RestApi.Entities
+{
+    public class Location : Base
+    {
+        [Key]
+        public long LocationId { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string LocationName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        [StringLength(150)]
+        public string FullAddress { get; set; }
+        public byte Note { get; set; }
+
+
+        public ICollection<Tour> Tours { get; set; }
+        public ICollection<Attraction> Attractions { get; set; }
+    }
+}
