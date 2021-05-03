b4a93dadbd46815e9ea7db284b3fa8285e194679
Alexx73 <ttllcba@gmail.com>
2021-05-02 19:58:50 -0700

    Frontend Solo

--- viajes365-backend/Viajes365RestApi/Dtos/RoleDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Viajes365RestApi.Dtos
-{
-    public class RoleDto
-    {
-        public string RoleName { get; set; }
-        public bool Active { get; set; }
-    }
-}
--- viajes365-frontend-responsive/src/app/_models/role.ts
+++
@@ -1,5 +0,0 @@
-export class Role {
-    roleId!: number;
-    roleName!: string;
-    active!: boolean
-}
