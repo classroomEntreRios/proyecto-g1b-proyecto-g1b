5d4adec154911249da2e94a9a2709c8a03a0ef0a
Anibal H Flores <floresanibal@yahoo.com.ar>
2021-05-10 19:07:50 -0300

    fix style delete-dialog.component.ts

--- viajes365-frontend/src/app/confirm-dialog/delete-dialog.component.ts
+++ viajes365-frontend/src/app/confirm-dialog/delete-dialog.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
-  styleUrls: ['./delete-dialog.component.scss']
 })
 export class DeleteDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
