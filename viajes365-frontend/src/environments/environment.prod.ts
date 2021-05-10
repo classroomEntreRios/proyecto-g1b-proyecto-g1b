90691feb30f95a8020f689850e4ec050a0f37d2a
Alexx73 <ttllcba@gmail.com>
2021-05-10 10:22:36 -0700

    deployed a firebase

--- viajes365-frontend/src/environments/environment.prod.ts
+++ viajes365-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://viajes365.azurewebsites.net/api'
+  // apiUrl: 'https://viajes365.azurewebsites.net/api',
+
+  apiUrl: 'http://localhost:5000/api',
+
 };
